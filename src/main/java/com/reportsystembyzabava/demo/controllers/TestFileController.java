357794675c19f7f35098c9b3e6b053b2d4d13ac2
thealeshka <alexfenin1234@gmail.com>
2019-10-05 02:44:16 +0300

    test Controller

---
+++ src/main/java/com/reportsystembyzabava/demo/controllers/TestFileController.java
@@ -0,0 +1,22 @@
+package com.reportsystembyzabava.demo.controllers;
+
+
+import com.reportsystembyzabava.demo.jpaRepositorys.ChatJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TestFileController {
+    @Autowired
+    private ChatJpaRepository chatJpaRepository;
+
+    public TestFileController() {
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String start() {
+        return "Start.html";
+    }
+}
