ee9919af74c0a6ed2ec84b578dc727b88af6cfef
thealeshka <alexfenin1234@gmail.com>
2019-10-05 02:04:29 +0300

    Initial commit

---
+++ src/main/java/com/reportsystembyzabava/demo/ReportsystembyzabavaApplication.java
@@ -0,0 +1,13 @@
+package com.reportsystembyzabava.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsystembyzabavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportsystembyzabavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reportsystembyzabava/demo/ReportsystembyzabavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reportsystembyzabava.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportsystembyzabavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
