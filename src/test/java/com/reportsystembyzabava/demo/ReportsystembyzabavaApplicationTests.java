cbd8e945140086cdaab0ddf3a6e533e316b939f0
thealeshka <alexfenin1234@gmail.com>
2019-10-07 13:11:09 +0300

    test for travis test

--- src/test/java/com/reportsystembyzabava/demo/ReportsystembyzabavaApplicationTests.java
+++ src/test/java/com/reportsystembyzabava/demo/ReportsystembyzabavaApplicationTests.java
@@ -3,11 +3,20 @@ package com.reportsystembyzabava.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class ReportsystembyzabavaApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
+
+    @Test
+    void test() {
+        assertEquals(0, 0);
+    }
+
 
 }
