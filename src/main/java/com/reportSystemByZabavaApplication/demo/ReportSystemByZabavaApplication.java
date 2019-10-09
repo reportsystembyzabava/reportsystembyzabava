37c41ec2273de85c73dc29c3fbb5b5ba17d13b26
thealeshka <alexfenin1234@gmail.com>
2019-10-10 01:06:51 +0300

    add test Logger

--- src/main/java/com/reportSystemByZabavaApplication/demo/ReportSystemByZabavaApplication.java
+++ src/main/java/com/reportSystemByZabavaApplication/demo/ReportSystemByZabavaApplication.java
@@ -1,13 +1,18 @@
 package com.reportSystemByZabavaApplication.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReportSystemByZabavaApplication {
+    private static final Logger logger = LoggerFactory.getLogger(ReportSystemByZabavaApplication.class);
 
     public static void main(String[] args) {
+        logger.info("Application starting");
         SpringApplication.run(ReportSystemByZabavaApplication.class, args);
+        logger.info("Application has started");
     }
 
 }
